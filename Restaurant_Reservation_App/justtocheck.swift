8d6770bf5555886af3ab8fb680e21544a2f34987
yuji090 <bishtakhil04@gmail.com>
2024-08-07 17:45:37 +0530

    just to check

---
+++ Restaurant_Reservation_App/justtocheck.swift
@@ -0,0 +1,18 @@
+//
+//  justtocheck.swift
+//  Restaurant_Reservation_App
+//
+//  Created by Akhil Bisht on 07/08/24.
+//
+
+import SwiftUI
+
+struct justtocheck: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    justtocheck()
+}
