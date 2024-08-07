9252058f140fe417846bfdc7c4f9424ac0816124
yuji090 <bishtakhil04@gmail.com>
2024-08-07 17:21:48 +0530

    Initial Commit

---
+++ Restaurant_Reservation_App/t.swift
@@ -0,0 +1,18 @@
+//
+//  t.swift
+//  Restaurant_Reservation_App
+//
+//  Created by Akhil Bisht on 07/08/24.
+//
+
+import SwiftUI
+
+struct t: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    t()
+}
